07d64231af17bc55609cc03c79a533b64717f938
ander zugasti <ander.zugasti@ikasle.egibide.org>
2020-01-18 10:23:28 +0100

    initial commit

---
+++ ProyectoEjercicio/ProyectoEjercicio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProyectoEjercicio
+//
+//  Created by ander zugasti on 18/01/2020.
+//  Copyright © 2020 ander zugasti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
