c2c5f4c5dbc4e4dce1050d13246800fe4af935b1
ander zugasti <ander.zugasti@ikasle.egibide.org>
2020-02-07 18:11:02 +0100

    casi casi datos de usuario
    el realm no, el min por km no

--- ProyectoEjercicio/ProyectoEjercicio/DatosPersonalesViewController.swift
+++ ProyectoEjercicio/ProyectoEjercicio/DatosPersonalesViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class DatosPersonalesViewController: UIViewController {
     @IBOutlet weak var alturatxt: UITextField!
     @IBOutlet weak var pesotxt: UITextField!
-   
+    @IBOutlet weak var estadoActual: UILabel!
+    
     @IBOutlet weak var gorduralbl: UILabel!
     
     @IBOutlet weak var objetivoAndando: UITextField!
@@ -21,18 +22,55 @@ class DatosPersonalesViewController: UIViewController {
     
     override func viewDidLoad() {
     super.viewDidLoad()
+        if ((UserDefaults.standard.string(forKey: "Altura")) != nil){
+            alturatxt.text = UserDefaults.standard.string(forKey: "Altura")
+            alturatxt.isUserInteractionEnabled = false
+        }
+         if ((UserDefaults.standard.string(forKey: "objetivoAndando")) != nil){
+             objetivoAndando.text = UserDefaults.standard.string(forKey: "objetivoAndando")
+             objetivoAndando.isUserInteractionEnabled = false
+        }
+           if ((UserDefaults.standard.string(forKey: "objetivoCorrer")) != nil){
+            objetivoCorrer.text = UserDefaults.standard.string(forKey: "objetivoCorrer")
+            objetivoCorrer.isUserInteractionEnabled = false
+        }
+            if ((UserDefaults.standard.string(forKey: "objetivoBici")) != nil){
+            objetivoBici.text = UserDefaults.standard.string(forKey: "objetivoBici")
+            objetivoBici.isUserInteractionEnabled = false
+        }
+           
+            pesotxt.text = UserDefaults.standard.string(forKey: "Peso")
+            print(UserDefaults.standard.string(forKey: "objetivoCorrer"))
+            print(UserDefaults.standard.string(forKey: "objetivoAndando")!)
+            print(UserDefaults.standard.dictionaryRepresentation().keys)
+            
+        if ((UserDefaults.standard.string(forKey: "Altura")) != nil && (UserDefaults.standard.string(forKey: "Peso")) != nil){
         
+               
+        }
     }
     
     @IBAction func Guardar_Editar(_ sender: Any) {
-        
+         if ((UserDefaults.standard.string(forKey: "Altura")) == nil){
+           def.set(alturatxt.text, forKey: "Altura")
+        }
+         if ((UserDefaults.standard.string(forKey: "objetivoAndando")) == nil){
+            def.set(objetivoAndando.text,forKey:"objetivoAndando")
+        }
+         if ((UserDefaults.standard.string(forKey: "objetivoCorrer")) == nil){
+            def.set(objetivoCorrer, forKey: "objetivoCorrer")
+        }
+         if ((UserDefaults.standard.string(forKey: "objetivoBici")) == nil){
+             def.set(objetivoBici, forKey: "objetivoBici")
+        }
         
             
-            def.set(alturatxt.text, forKey: "Altura")
+            
             def.set(pesotxt.text, forKey: "Peso")
-            def.set(objetivoAndando.text,forKey:"objetivoAndando")
-            def.set(objetivoCorrer, forKey: "objetivoCorrer")
-            def.set(objetivoBici, forKey: "objetivoBici")
+            
+            
+           
+            def.synchronize()
             
         
         
